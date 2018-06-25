647a552490c7b554bf323ede35c33913b1c08441
louisguitton <louisguitton93@gmail.com>
2018-06-25 23:48:34 +0200

    added user login and profile page

--- app/models.py
+++ app/models.py
@@ -1,21 +1,44 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
+
 from app import db
+from app import login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(120), index=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     location = db.Column(db.String(120), index=True)
     linkedin_handle = db.Column(db.String(64), index=True, unique=True)
-    # password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # cv
     # projects as another table
 
     videos = db.relationship('Video', backref='applicant', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)    
+        return '<User {}>'.format(self.username)    
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Question(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,74 @@
 from flask import render_template, request, flash, redirect, url_for
 from logging import Formatter, FileHandler
 import logging
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
+from app.models import User
 from app import app
-from forms import ApplyForm, LoginForm, RegisterForm, ForgotForm
+from app.forms import ApplyForm, LoginForm, RegistrationForm, ForgotForm
+from app import db
 import config
 
 
 @app.route('/')
-def home():
+def index():
     return render_template('pages/placeholder.home.html', config=config)
 
+@app.route('/about')
+def about():
+    return render_template('pages/placeholder.about.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next') # url_for ?
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('forms/login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data.lower(), email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('forms/register.html', title='Register', form=form)
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('pages/user.html', user=user)
+
 @app.route('/apply', methods=['GET', 'POST'])
+@login_required
 def apply():
     form = ApplyForm()
     # if form.validate_on_submit():
@@ -20,35 +77,11 @@ def apply():
             form.name.data, form.email.data)
         app.logger.info(msg)
         flash(msg)
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
     return render_template('forms/apply.html', global_data=config.global_data, form=form)
 
 
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
 # Error handlers.
-
-
 @app.errorhandler(500)
 def internal_error(error):
     #db_session.rollback()
