87b6c35ae10c3f4223c3fcbc6c3667d0c5df2600
louisguitton <louisguitton93@gmail.com>
2018-06-25 20:26:06 +0200

    created db migrations folder and created user table

--- app/models.py
+++ app/models.py
@@ -1,17 +1,30 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy import Column, Integer, String
-# from app import db
+from app import db
 
-engine = create_engine('sqlite:///database.db', echo=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+# engine = create_engine('sqlite:///database.db', echo=True)
+# db_session = scoped_session(sessionmaker(autocommit=False,
+#                                          autoflush=False,
+#                                          bind=engine))
+# Base = declarative_base()
+# Base.query = db_session.query_property()
 
 # Set your classes here.
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    location = db.Column(db.String(120), index=True)
+    linkedin_handle = db.Column(db.String(64), index=True, unique=True)
+    # password_hash = db.Column(db.String(128))
+    # cv
+    # projects as another table
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)    
 
 '''
 class User(Base):
@@ -27,5 +40,39 @@ class User(Base):
         self.password = password
 '''
 
+'''
+# Video
+video_id
+user_id
+question_id
+date
+S3_url
+has_audio
+has_video
+max_duration
+duration
+size
+device_id
+
+# Submission
+submission_id
+user_id
+creation_date (beginning of session)
+submission_date (end of session)
+state (0, 1, 2, 3)
+videos = array of video_id
+
+# Question
+question_id
+text
+
+# Device
+user_agent
+browser
+version%  
+'''
+
+
 # Create tables.
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
+
