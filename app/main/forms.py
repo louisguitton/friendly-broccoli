66abf73de98954efc9441db3caf86dd1dfd6deb6
louisguitton <louisguitton93@gmail.com>
2018-07-24 20:22:33 +0200

    fixes here and there

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 
 class ApplyForm(FlaskForm):
-    name = TextField('Your Name', validators=[DataRequired(), Length(min=6, max=25)])
-    location = TextField('Location', validators=[DataRequired(), Length(min=6, max=40)])
-    linkedin_url = TextField('Linkedin', validators=[DataRequired(), Length(min=6, max=40)])
+    name = TextField('Your Name', validators=[DataRequired(), Length(min=6, max=128)])
+    location = TextField('Location', validators=[DataRequired(), Length(min=6, max=128)])
+    linkedin_url = TextField('Linkedin', validators=[DataRequired(), Length(min=6, max=128)])
     submit = SubmitField('Submit')
