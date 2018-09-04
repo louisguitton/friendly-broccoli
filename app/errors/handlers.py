e515f7521f7b7dc212319a42e3c6f143242b0bee
louisguitton <louisguitton93@gmail.com>
2018-09-04 14:45:12 +0200

    added template to debug csrf error

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,4 +1,6 @@
 from flask import render_template, current_app
+from flask_wtf.csrf import CSRFError
+
 from app import db
 from app.errors import bp
 
@@ -24,5 +26,10 @@ def not_found_error(error):
 
 
 @bp.app_errorhandler(403)
-def not_found_error(error):
+def forbidden(error):
     return render_template('errors/403.html'), 403
+
+
+@bp.errorhandler(CSRFError)
+def csrf_error(e):
+    return render_template('errors/csrf_error.html', reason=e.description), 400
